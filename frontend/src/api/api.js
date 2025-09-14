9629def3536f687c9fce318d0f1992bf8d84057e
Riham203 <137186655+Riham203@users.noreply.github.com>
2025-09-14 23:44:28 +0530

    Update api

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,12 +1,15 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: "https://feedback-app-ssly.onrender.com/api", 
+  withCredentials: true,
 });
 
 API.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
